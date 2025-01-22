03a3232ae22e9b890d06117c61f8a5377947095a
eugene <eugenebokhan@icloud.com>
2025-01-22 14:32:46 +0000

    naming fix

--- convert_model.py
+++ convert_model.py
@@ -7,13 +7,13 @@ from typer import run
 from fartsovka.importers.executorch.importer import ExecutorchModel
 from fartsovka.importers.executorch.importer import import_model as import_et
 
-EXPORT_DTYPE = jnp.bfloat16
+ACTIVATION_EXPORT_TYPE = jnp.bfloat16
 
 
 def main(output_dir: Path) -> None:
     model = import_et(
         ExecutorchModel.LLAMA32_1B_INSTRUCT_QLORA,
-        activation_precision=EXPORT_DTYPE,
+        activation_precision=ACTIVATION_EXPORT_TYPE,
     )
     weights = model.export_weights()
     save_file(weights, output_dir / "fs_model.safetensors")
