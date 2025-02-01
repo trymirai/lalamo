5cc9189b411ecdddde5be9a42cda54ab8532ebc1
Artur Chakhvadze <norpadon@yandex.ru>
2025-02-01 20:53:34 +0400

    Make Ruff happy.

--- fartsovka/modules/linear.py
+++ fartsovka/modules/linear.py
@@ -359,8 +359,8 @@ class QLoRALinear(GroupQuantizedLinearBase["QLoRALinearConfig"]):
         super().__post_init__()
         if self.lora_down_weights.dtype != self.config.activation_precision:
             raise ValueError(
-                f"LORA down weight dtype ({self.lora_down_weights.dtype}) is not equal to specified activation precision"
-                f" ({self.config.activation_precision}).",
+                f"LORA down weight dtype ({self.lora_down_weights.dtype}) is not equal to the"
+                f" specified activation precision ({self.config.activation_precision}).",
                 " Quantized layers require parameter dtypes to be equal to the activation precision.",
             )
         lora_down_output_dim, lora_down_input_dim = self.lora_down_weights.shape
