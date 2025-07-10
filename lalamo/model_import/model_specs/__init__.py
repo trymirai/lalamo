c8c79a9258425bd3ed6cfcdd3e40018315b39b0e
Aleksei Savin <savin.alexey.m@gmail.com>
2025-07-10 19:28:08 +0100

    Update quantization list

--- lalamo/model_import/model_specs/__init__.py
+++ lalamo/model_import/model_specs/__init__.py
@@ -36,7 +36,6 @@ def build_quantized_models(model_specs: list[ModelSpec]):
         "Qwen/Qwen2.5-7B-Instruct",
         "Qwen/Qwen2.5-Coder-3B-Instruct",
         "Qwen/Qwen2.5-Coder-7B-Instruct",
-        "Qwen/Qwen3-1.7B",
         "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B",
         "HuggingFaceTB/SmolLM2-1.7B-Instruct",
         "meta-llama/Llama-3.2-3B-Instruct",
