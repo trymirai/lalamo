198dcf2c978f6fdbd92fbf826475b7230f9c45c8
Artur Chakhvadze <norpadon@yandex.ru>
2025-09-02 19:34:54 +0400

    Disable executorch stuff.

--- lalamo/model_import/configs/__init__.py
+++ lalamo/model_import/configs/__init__.py
@@ -1,5 +1,6 @@
 from .common import ForeignConfig
-from .executorch import ETLlamaConfig
+
+# from .executorch import ETLlamaConfig
 from .huggingface import (
     HFGemma2Config,
     HFGemma3Config,
@@ -11,7 +12,7 @@ from .huggingface import (
 )
 
 __all__ = [
-    "ETLlamaConfig",
+    # "ETLlamaConfig",
     "ForeignConfig",
     "HFGemma2Config",
     "HFGemma3Config",
--- lalamo/model_import/model_specs/llama.py
+++ lalamo/model_import/model_specs/llama.py
@@ -1,7 +1,6 @@
 from dataclasses import replace
 
-from lalamo.model_import.configs import ETLlamaConfig, HFLlamaConfig
-from lalamo.quantization import QuantizationMode
+from lalamo.model_import.configs import HFLlamaConfig
 
 from .common import (
     HUGGINFACE_GENERATION_CONFIG_FILE,
@@ -54,20 +53,20 @@ LLAMA32 = [
         tokenizer_files=(*HUGGINGFACE_TOKENIZER_FILES, HUGGINFACE_GENERATION_CONFIG_FILE),
         use_cases=tuple(),
     ),
-    ModelSpec(
-        vendor="Meta",
-        family="Llama-3.2",
-        name="Llama-3.2-1B-Instruct-QLoRA",
-        size="1B",
-        quantization=QuantizationMode.UINT4,
-        repo="meta-llama/Llama-3.2-1B-Instruct-QLORA_INT4_EO8",
-        config_type=ETLlamaConfig,
-        config_file_name="params.json",
-        weights_file_names=("consolidated.00.pth",),
-        weights_type=WeightsType.TORCH,
-        tokenizer_files=_tokenizer_files_from_another_repo("meta-llama/Llama-3.2-1B-Instruct"),
-        use_cases=tuple(),
-    ),
+    # ModelSpec(
+    #     vendor="Meta",
+    #     family="Llama-3.2",
+    #     name="Llama-3.2-1B-Instruct-QLoRA",
+    #     size="1B",
+    #     quantization=QuantizationMode.UINT4,
+    #     repo="meta-llama/Llama-3.2-1B-Instruct-QLORA_INT4_EO8",
+    #     config_type=ETLlamaConfig,
+    #     config_file_name="params.json",
+    #     weights_file_names=("consolidated.00.pth",),
+    #     weights_type=WeightsType.TORCH,
+    #     tokenizer_files=_tokenizer_files_from_another_repo("meta-llama/Llama-3.2-1B-Instruct"),
+    #     use_cases=tuple(),
+    # ),
     ModelSpec(
         vendor="Meta",
         family="Llama-3.2",
@@ -82,20 +81,20 @@ LLAMA32 = [
         tokenizer_files=(*HUGGINGFACE_TOKENIZER_FILES, HUGGINFACE_GENERATION_CONFIG_FILE),
         use_cases=tuple(),
     ),
-    ModelSpec(
-        vendor="Meta",
-        family="Llama-3.2",
-        name="Llama-3.2-3B-Instruct-QLoRA",
-        size="3B",
-        quantization=QuantizationMode.UINT4,
-        repo="meta-llama/Llama-3.2-3B-Instruct-QLORA_INT4_EO8",
-        config_type=ETLlamaConfig,
-        config_file_name="params.json",
-        weights_file_names=("consolidated.00.pth",),
-        tokenizer_files=_tokenizer_files_from_another_repo("meta-llama/Llama-3.2-3B-Instruct"),
-        weights_type=WeightsType.TORCH,
-        use_cases=tuple(),
-    ),
+    # ModelSpec(
+    #     vendor="Meta",
+    #     family="Llama-3.2",
+    #     name="Llama-3.2-3B-Instruct-QLoRA",
+    #     size="3B",
+    #     quantization=QuantizationMode.UINT4,
+    #     repo="meta-llama/Llama-3.2-3B-Instruct-QLORA_INT4_EO8",
+    #     config_type=ETLlamaConfig,
+    #     config_file_name="params.json",
+    #     weights_file_names=("consolidated.00.pth",),
+    #     tokenizer_files=_tokenizer_files_from_another_repo("meta-llama/Llama-3.2-3B-Instruct"),
+    #     weights_type=WeightsType.TORCH,
+    #     use_cases=tuple(),
+    # ),
 ]
 
 LLAMA_MODELS = LLAMA31 + LLAMA32
