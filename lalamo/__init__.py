68d65588da64ee79f6ba7b2df79e041a552b9a32
Aleksei Savin <savin.alexey.m@gmail.com>
2025-09-16 22:51:17 +0100

    Revert manual version up

--- lalamo/__init__.py
+++ lalamo/__init__.py
@@ -1,7 +1,7 @@
 from lalamo.model_import import REPO_TO_MODEL, ModelSpec, import_model
 from lalamo.modules import Decoder
 
-__version__ = "0.3.4"
+__version__ = "0.3.3"
 
 __all__ = [
     "REPO_TO_MODEL",
