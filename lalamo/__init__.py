d0eeae15587eccd5d56adae4fb8211cf085d3eea
Vladimir W <matterai@users.noreply.github.com>
2025-11-28 11:09:52 -0300

    Exported speculator commands out of the box to call them externally (#69)
    * exported speculator command out of the box to call them externally
    
    * estimator return int

--- lalamo/__init__.py
+++ lalamo/__init__.py
@@ -9,19 +9,31 @@ from lalamo.message_processor import (
 )
 from lalamo.model_import import ModelSpec, import_model
 from lalamo.models import LanguageModel, Router
+from lalamo.speculator import (
+    CollectTracesEvent,
+    SpeculatorTrainingEvent,
+    estimate_batchsize_from_memory,
+    inference_collect_traces,
+    train_speculator,
+)
 
 __version__ = "0.5.3"
 
 __all__ = [
     "AssistantMessage",
+    "CollectTracesEvent",
     "ContentBlock",
     "Image",
     "LanguageModel",
     "Message",
     "ModelSpec",
     "Router",
+    "SpeculatorTrainingEvent",
     "SystemMessage",
     "ToolSchema",
     "UserMessage",
+    "estimate_batchsize_from_memory",
     "import_model",
+    "inference_collect_traces",
+    "train_speculator",
 ]
