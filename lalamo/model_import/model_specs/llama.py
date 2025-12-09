bbe4cd3c3a012961d5abee5daa53d46cc55e99d7
uuuvn <uuuvn@proton.me>
2025-12-10 01:58:27 +0400

    llama mlx

--- lalamo/model_import/model_specs/llama.py
+++ lalamo/model_import/model_specs/llama.py
@@ -1,6 +1,7 @@
 from lalamo.model_import.decoder_configs import HFLlamaConfig
+from lalamo.quantization import QuantizationMode
 
-from .common import ModelSpec
+from .common import ConfigMap, FileSpec, ModelSpec
 
 __all__ = ["LLAMA_MODELS"]
 
@@ -14,6 +15,17 @@ LLAMA31 = [
         repo="meta-llama/Llama-3.1-8B-Instruct",
         config_type=HFLlamaConfig,
         use_cases=tuple(),
+    ),
+    ModelSpec(
+        vendor="Meta",
+        family="Llama-3.1",
+        name="Llama-3.1-8B-Instruct-4bit",
+        size="8B",
+        quantization=QuantizationMode.UINT4,
+        repo="mlx-community/Llama-3.1-8B-Instruct-4bit",
+        config_type=HFLlamaConfig,
+        use_cases=tuple(),
+        configs=ConfigMap(generation_config=FileSpec("generation_config.json", "meta-llama/Llama-3.1-8B-Instruct")),
     ),
 ]
 
@@ -32,11 +44,55 @@ LLAMA32 = [
     ModelSpec(
         vendor="Meta",
         family="Llama-3.2",
+        name="Llama-3.2-1B-Instruct-4bit",
+        size="1B",
+        quantization=QuantizationMode.UINT4,
+        repo="mlx-community/Llama-3.2-1B-Instruct-4bit",
+        config_type=HFLlamaConfig,
+        configs=ConfigMap(generation_config=FileSpec("generation_config.json", "meta-llama/Llama-3.2-1B-Instruct")),
+        use_cases=tuple(),
+    ),
+    ModelSpec(
+        vendor="Meta",
+        family="Llama-3.2",
+        name="Llama-3.2-1B-Instruct-8bit",
+        size="1B",
+        quantization=QuantizationMode.UINT8,
+        repo="mlx-community/Llama-3.2-1B-Instruct-8bit",
+        config_type=HFLlamaConfig,
+        configs=ConfigMap(generation_config=FileSpec("generation_config.json", "meta-llama/Llama-3.2-1B-Instruct")),
+        use_cases=tuple(),
+    ),
+    ModelSpec(
+        vendor="Meta",
+        family="Llama-3.2",
         name="Llama-3.2-3B-Instruct",
         size="3B",
         quantization=None,
         repo="meta-llama/Llama-3.2-3B-Instruct",
         config_type=HFLlamaConfig,
+        use_cases=tuple(),
+    ),
+    ModelSpec(
+        vendor="Meta",
+        family="Llama-3.2",
+        name="Llama-3.2-3B-Instruct-4bit",
+        size="3B",
+        quantization=QuantizationMode.UINT4,
+        repo="mlx-community/Llama-3.2-3B-Instruct-4bit",
+        config_type=HFLlamaConfig,
+        configs=ConfigMap(generation_config=FileSpec("generation_config.json", "meta-llama/Llama-3.2-3B-Instruct")),
+        use_cases=tuple(),
+    ),
+    ModelSpec(
+        vendor="Meta",
+        family="Llama-3.2",
+        name="Llama-3.2-3B-Instruct-8bit",
+        size="3B",
+        quantization=QuantizationMode.UINT8,
+        repo="mlx-community/Llama-3.2-3B-Instruct-8bit",
+        config_type=HFLlamaConfig,
+        configs=ConfigMap(generation_config=FileSpec("generation_config.json", "meta-llama/Llama-3.2-3B-Instruct")),
         use_cases=tuple(),
     ),
 ]
