571e9997a265ffdc0c0d5930b64ac05f64046df6
uuuvn <uuuvn@proton.me>
2026-01-06 19:56:29 +0400

    LFM2.5 config quirks

--- lalamo/model_import/model_specs/lfm2.py
+++ lalamo/model_import/model_specs/lfm2.py
@@ -9,22 +9,22 @@ from .common import ConfigMap, FileSpec, ModelSpec
 __all__ = ["LFM2_MODELS"]
 
 
-def _lfm2_repo(size: str, variant: str | None, quantization: QuantizationMode | None) -> tuple[str, str]:
+def _lfm_repo(family: str, size: str, variant: str | None, quantization: QuantizationMode | None) -> tuple[str, str]:
     return (
         "LiquidAI" if quantization is None else "mlx-community",
-        f"LFM2-{size}"
+        f"{family}-{size}"
         f"{f'-{variant}' if variant is not None else ''}"
         f"{f'-{quantization.bits}bit' if quantization is not None else ''}",
     )
 
 
-LFM2_MODELS = [
+_LFM20_MODELS = [
     ModelSpec(
         vendor="LiquidAI",
         family="LFM2",
-        name=_lfm2_repo(size, variant, quantization)[1],
+        name=_lfm_repo("LFM2", size, variant, quantization)[1],
         size=size,
-        repo="/".join(_lfm2_repo(size, variant, quantization)),
+        repo="/".join(_lfm_repo("LFM2", size, variant, quantization)),
         config_type=HFLFM2Config,
         quantization=quantization,
         configs=ConfigMap(
@@ -39,3 +39,25 @@ LFM2_MODELS = [
         product(["2.6B"], ["Exp"], [QuantizationMode.UINT4, QuantizationMode.UINT8]),
     )
 ]
+
+_LFM25_MODELS = [
+    ModelSpec(
+        vendor="LiquidAI",
+        family="LFM2.5",
+        name=_lfm_repo("LFM2.5", size, variant, quantization)[1],
+        size=size,
+        repo="/".join(_lfm_repo("LFM2.5", size, variant, quantization)),
+        config_type=HFLFM2Config,
+        quantization=quantization,
+        configs=ConfigMap(
+            generation_config=GenerationConfig(temperature=0.1, top_k=50, top_p=0.1),  # , repetition_penalty=1.05
+            chat_template=FileSpec("chat_template.jinja"),
+        ),
+        use_cases=tuple(),
+    )
+    for size, variant, quantization in chain(
+        product(["1.2B"], ["Instruct"], [None]),
+    )
+]
+
+LFM2_MODELS = _LFM20_MODELS + _LFM25_MODELS
