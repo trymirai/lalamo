8ce34606610c35aa86fbd1d5d2c254ce71a56c3a
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2026-02-03 13:31:07 +0300

    refactor(evals): move eval specs to evals/datasets/specs

---
+++ lalamo/evals/datasets/specs/__init__.py
@@ -0,0 +1,10 @@
+from .base import EvalSpec
+from .mmlu import MMLU_PRO
+
+# Build registry
+REPO_TO_EVAL = {
+    spec.repo: spec
+    for spec in [MMLU_PRO]
+}
+
+__all__ = ["EvalSpec", "REPO_TO_EVAL"]
