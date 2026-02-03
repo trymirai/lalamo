c0d2e5cfbb38ec4621000835b1c40db9824e4313
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2026-02-03 13:35:01 +0300

    refactor(evals): move dataset converter to evals/datasets

---
+++ lalamo/evals/datasets/__init__.py
@@ -0,0 +1,9 @@
+from .converter import EvalConversionCallbacks, import_eval
+from .specs import EvalSpec, REPO_TO_EVAL
+
+__all__ = [
+    "EvalConversionCallbacks",
+    "import_eval",
+    "EvalSpec",
+    "REPO_TO_EVAL",
+]
