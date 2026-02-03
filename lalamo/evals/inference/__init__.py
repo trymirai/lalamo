e9ccc32d617c8c44d6360f5ddf9a667340d819b5
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2026-02-03 13:36:32 +0300

    refactor(evals): move inference runner to evals/inference

---
+++ lalamo/evals/inference/__init__.py
@@ -0,0 +1,8 @@
+from .engines import InferenceEngine, LalamoInferenceEngine
+from .runner import run_inference
+
+__all__ = [
+    "InferenceEngine",
+    "LalamoInferenceEngine",
+    "run_inference",
+]
