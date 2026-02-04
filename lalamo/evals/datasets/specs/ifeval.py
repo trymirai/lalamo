15d16a00847a4aa1aa1a933f854b76488aeb4d17
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2026-02-04 16:51:23 +0300

    refactor(evals): use absolute imports and GitHub evals dependency

---
+++ lalamo/evals/datasets/specs/ifeval.py
@@ -0,0 +1,9 @@
+from evals import IFEvalAdapter
+from lalamo.evals.datasets.specs.base import EvalSpec
+
+IFEVAL = EvalSpec(
+    name="IFEval",
+    repo="google/IFEval",
+    splits=["train"],
+    handler_type=IFEvalAdapter,
+)
