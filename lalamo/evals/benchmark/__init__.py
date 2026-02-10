10e30d70096ab4143876fb455fdeac94d00b00e9
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2026-02-10 12:15:35 +0300

    refactor(evals): export benchmark public API

--- lalamo/evals/benchmark/__init__.py
+++ lalamo/evals/benchmark/__init__.py
@@ -1,1 +1,8 @@
-__all__ = []
+from lalamo.evals.benchmark.callbacks import BaseBenchmarkCallbacks, ConsoleCallbacks
+from lalamo.evals.benchmark.command import benchmark_command_handler
+
+__all__ = [
+    "BaseBenchmarkCallbacks",
+    "ConsoleCallbacks",
+    "benchmark_command_handler",
+]
