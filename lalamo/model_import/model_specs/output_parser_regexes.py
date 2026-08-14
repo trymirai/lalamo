af4d91a348ce64ad40e156b0757ef6ed25369d35
Roman Knyazhitskiy <git@knyaz.tech>
2026-08-14 17:45:24 +0200

    Old parsing blows up on multi-message
    this is temporary fix, working on the proper one rn

--- lalamo/model_import/model_specs/output_parser_regexes.py
+++ lalamo/model_import/model_specs/output_parser_regexes.py
@@ -1,2 +1,6 @@
 OPTIONAL_THINKING_OUTPUT_PARSER_REGEX = r"(?s)(?:<think>)?(?P<chain_of_thought>.*?)(?:</think>\s*(?P<response>.*))?\Z"
-GPT_OSS_OUTPUT_PARSER_REGEX = r"(?s)analysis(?P<chain_of_thought>.*?)(?:assistantfinal(?P<response>.*))?\Z"
+GPT_OSS_OUTPUT_PARSER_REGEX = (
+    r"(?s)(?:<\|channel\|>analysis<\|message\|>(?P<chain_of_thought>.*?))?"
+    r"(?:(?:<\|end\|><\|start\|>assistant)?<\|channel\|>final<\|message\|>(?P<response>.*?))?"
+    r"(?:<\|return\|>|<\|end\|>)?\Z"
+)
